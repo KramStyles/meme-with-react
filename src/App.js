b9606b7fcc159c9683eb3d120364b3426094e68e
kramstyles <mark.eke@decagon.dev>
2022-02-07 21:06:59 +0100

    working on header.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+import Top from './Components/Header'
+import Body from './Components/Body'
+
+export default function App(){
+  return (
+    <main>
+      <Top />
+      <Body />
+    </main>
+  )
+}
+
+
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header>
+            <div>Hello</div>
+            <div>Hi</div>
+        </header>
+    )
+}
+
+export default Header

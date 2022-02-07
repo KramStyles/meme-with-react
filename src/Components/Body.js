878a807da68a4777877d746d68d32c87b2f0f043
kramstyles <mark.eke@decagon.dev>
2022-02-07 21:28:40 +0100

    working on body

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -3,7 +3,13 @@ import React from "react";
 export default function Body(){
     return(
         <div className="container">
-            
+            <form>
+                <div className="inputHolder">
+                    <input placeholder="Upper text" id="txtUpper"/>
+                    <input placeholder="Lower text" id="txtLower"/>
+                </div>
+                <button>Get a new meme Image</button>
+            </form>
         </div>
     )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,8 +3,12 @@ import React from "react";
 function Header(){
     return(
         <header>
-            <div>Hello</div>
-            <div>Hi</div>
+            <div>
+                <h1><i class="fab fa-pied-piper-alt"></i> Meme Generator</h1>
+            </div>
+            <div>
+                <h3>React Course - Project 3</h3>
+            </div>
         </header>
     )
 }

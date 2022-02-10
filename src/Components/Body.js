fbf5e1265b99e5ad324851cac7067af98150ab51
kramstyles <mark.eke@decagon.dev>
2022-02-10 16:51:49 +0100

    made meme show on button click

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,4 +1,6 @@
 import React from "react";
+import memesdata from "../memesdata";
+
 
 export default function Body(){
     function saySomething(){
@@ -6,7 +8,19 @@ export default function Body(){
     }
 
     function handleClick() {
-        console.log("I was clicked!")
+        const memes = memesdata.data.memes;
+        const randNum = Math.floor(Math.random() * memes.length);
+        const url = memes[randNum]['url'];
+        const {name} = memes[randNum];
+
+        let upper = document.getElementById('txtUpper').value.trim();
+        let lower = document.getElementById('txtLower').value.trim();
+        if(upper == '') upper = name;
+        if(lower == '') lower = name;
+
+        document.getElementsByClassName('imageContainer')[0].style.backgroundImage = `url(${url})`;
+        document.getElementById('txtTop').textContent = upper;
+        document.getElementById('txtBottom').textContent = lower;
     }
 
 
@@ -14,16 +28,16 @@ export default function Body(){
         <div className="container">
             <form>
                 <div className="inputHolder">
-                    <input placeholder="Upper text" id="txtUpper"/>
-                    <input placeholder="Lower text" id="txtLower"/>
+                    <input placeholder="Provide Upper Text" id="txtUpper"/>
+                    <input placeholder="Provide Lower Text" id="txtLower"/>
                 </div>
-                <button onClick={handleClick()} type="button">Click me</button>
+                <button onClick={handleClick} type="button">Click me</button>
                 {/*<button onClick={saySomething()} type="button">Get a new meme Image</button>*/}
             </form>
 
             <div className="imageContainer">
-                <article>Hello</article>
-                <article>Hi, i'm down here</article>
+                <article id="txtTop">Hello</article>
+                <article id="txtBottom">Hi, i'm down here</article>
             </div>
         </div>
     )

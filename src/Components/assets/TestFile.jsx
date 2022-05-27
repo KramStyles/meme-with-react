040289eaa210aa1377c62483b8e872549b593673
kramstyles <mark.eke@decagon.dev>
2022-05-27 21:45:39 +0100

    working with state to update addition to array 1

--- src/Components/assets/TestFile.jsx
+++ src/Components/assets/TestFile.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react';
+
+
 export default function TestFile() {
+    const thingsArray = ['Thing 1', 'Thing 2'];
+    const [things, setThings] = useState(thingsArray);
+
+    const thingsElement = things.map(item => <p key={item}>Thing: {item}</p>)
+
+    const addItem = () => {
+        let newItem = `Thing ${thingsArray.length + 1}`;
+        things.push(newItem);
+
+        console.log(things);
+    }
+
   return (
-    <div>TestFile</div>
+    <div className='container'>
+        <button onClick={addItem}>Add Item</button>
+        {thingsElement}
+    </div>
   )
 }

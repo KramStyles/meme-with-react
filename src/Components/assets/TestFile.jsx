afe7510f37c508b6fd985f5fc96888add269fba7
kramstyles <mark.eke@decagon.dev>
2022-05-27 22:21:10 +0100

    Added state to toggle yes and No

--- src/Components/assets/TestFile.jsx
+++ src/Components/assets/TestFile.jsx
@@ -5,19 +5,33 @@ export default function TestFile() {
     const thingsArray = ['Thing 1', 'Thing 2'];
     const [things, setThings] = useState(thingsArray);
 
+    const [stuff, setStuff] = useState('No');
+    const [active, setActive] = useState(false);
+
     const thingsElement = things.map(item => <p key={item}>Thing: {item}</p>)
-
+ 
     const addItem = () => {
-        let newItem = `Thing ${thingsArray.length + 1}`;
-        things.push(newItem);
+        let newItem = `Thing ${things.length + 1}`;
+        setThings(prev => [...prev, newItem])
 
-        console.log(things);
+    }
+
+    const handleClick = () => {
+        if (active) {
+            setStuff('No');
+            setActive(false);
+        } else{
+            setStuff('Yes');
+            setActive(true);
+        }
     }
 
   return (
     <div className='container'>
         <button onClick={addItem}>Add Item</button>
+        <button onClick={handleClick}>Change Stuff</button>
         {thingsElement}
+        <h3>{stuff}</h3>
     </div>
   )
 }
